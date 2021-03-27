6aa6babd070fdf28c71c1f17a2ac6edad5a011cc
dban3193 <70111525+dban3193@users.noreply.github.com>
2021-03-27 12:10:42 +0530

    Update Day2_Tip-calculator.py
    Added format function

--- Day2_Tip-calculator.py
+++ Day2_Tip-calculator.py
@@ -6,7 +6,8 @@ tip=int(input("what percentage tip would you like to give? 10,12, or 15? "))
 persons=int(input("how many persons to split the bill? "))
 
 total_after_tip=total+total*tip*0.01
-per_person=round(total_after_tip/persons,2)
-
+#per_person=round(total_after_tip/persons,2)
+#the above wont give accurate results if we have a clean division with no lingering.To fix that, we use format function
+per_person="{:.2f}.format(total_after_tip/persons)
 
 print(f"Each person should pay: ${per_person}")
